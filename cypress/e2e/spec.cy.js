a9aed82795150607b028285d737cd90aa23c0530
david <david@assertqa.com>
2024-05-19 21:40:22 +0200

    added version 1.0.2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
+import '../../cypress-shared/sharedCustomCommand'
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    cy.printCommandVersion()
   })
 })
